1c55645357594522ccb748f029c4319285b63e45
nicol <nicol@DESKTOP-4CM4HAU>
2024-09-20 15:47:23 -0500

    Add project files.

---
+++ loginBasico/Controllers/CuentaController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using loginBasico.Models;
+using System.Reflection.Metadata.Ecma335;
+namespace loginBasico.Controllers
+{
+    public class CuentaController : Controller
+    {
+        private static string usuario_V = "nicolas";
+        private static string password_V = "123456";
+
+        //para retornar vista
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(usuario usuario)
+        {
+            if (usuario.User == usuario_V && usuario.Pass == password_V)
+            {
+                return RedirectToAction("Index", "Home");
+                Console.WriteLine("funciona");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Usuario o contraseña incorrectos. Inténtalo de nuevo.";
+                return View(); 
+            }
+
+        }
+
+      
+    }
+}
---
+++ loginBasico/Models/usuario.cs
@@ -0,0 +1,8 @@
+﻿namespace loginBasico.Models
+{
+    public class usuario
+    {
+        public string User { get; set; }
+        public string Pass { get; set; }
+    }
+}
